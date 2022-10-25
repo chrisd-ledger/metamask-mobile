f0d5c048ef0c529f5c5efe43c5af7fa15a22ba6d
Owen Craston <owengc12@gmail.com>
2022-10-25 12:26:03 -0700

    parity with #3755

--- app/components/Nav/App/index.js
+++ app/components/Nav/App/index.js
@@ -428,10 +428,16 @@ const App = ({ selectedAddress, userLoggedIn }) => {
               component={OnboardingRootNav}
               options={{ headerShown: false }}
             />
-            {userLoggedIn && (
+            {userLoggedIn ? (
               <Stack.Screen
                 name={Routes.ONBOARDING.HOME_NAV}
                 component={Main}
+                options={{ headerShown: false }}
+              />
+            ) : (
+              <Stack.Screen
+                name="Login"
+                component={Login}
                 options={{ headerShown: false }}
               />
             )}

99606a87792902576af35aabe54084c65964e778
Owen Craston <owengc12@gmail.com>
2022-12-14 15:39:49 -0800

    use routes object

--- app/components/Nav/App/index.js
+++ app/components/Nav/App/index.js
@@ -181,7 +181,7 @@ const App = ({ selectedAddress, userLoggedIn }) => {
       try {
         if (existingUser && selectedAddress) {
           await Authentication.appTriggeredAuth(selectedAddress);
-          navigator.replace('HomeNav');
+          navigator.replace(Routes.ONBOARDING.HOME_NAV);
         }
         //Cancel auth if the existing user has not been set
       } catch (error) {
@@ -412,7 +412,7 @@ const App = ({ selectedAddress, userLoggedIn }) => {
             }}
           >
             <Stack.Screen
-              name="Login"
+              name={Routes.ONBOARDING.LOGIN}
               component={Login}
               options={{ headerShown: false }}
             />
@@ -423,7 +423,7 @@ const App = ({ selectedAddress, userLoggedIn }) => {
             />
             {userLoggedIn && (
               <Stack.Screen
-                name="HomeNav"
+                name={Routes.ONBOARDING.HOME_NAV}
                 component={Main}
                 options={{ headerShown: false }}
               />

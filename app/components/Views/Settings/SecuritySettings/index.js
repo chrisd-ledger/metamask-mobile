4fe7d3d496805364e8cb12f9270e88caf2ac7271
Owen Craston <owengc12@gmail.com>
2023-01-30 21:15:58 -0800

    remove debug logs

--- app/components/Views/Settings/SecuritySettings/index.js
+++ app/components/Views/Settings/SecuritySettings/index.js
@@ -352,11 +352,6 @@ class Settings extends PureComponent {
   };
 
   setPassword = async (enabled, passwordType) => {
-    console.log(
-      'vault/ SecuritySettings setPassword called with',
-      enabled,
-      passwordType,
-    );
     this.setState({ loading: true }, async () => {
       let credentials;
       try {
@@ -366,12 +361,8 @@ class Settings extends PureComponent {
       }
 
       if (credentials && credentials.password !== '') {
-        console.log('vault/ SecuritySettings setPassword has credentials');
         this.storeCredentials(credentials.password, enabled, passwordType);
       } else {
-        console.log(
-          'vault/ SecuritySettings setPassword does not have credentials, navigating to enter password',
-        );
         this.props.navigation.navigate('EnterPasswordSimple', {
           onPasswordSet: (password) => {
             this.storeCredentials(password, enabled, passwordType);
@@ -392,12 +383,6 @@ class Settings extends PureComponent {
   };
 
   storeCredentials = async (password, enabled, type) => {
-    console.log(
-      'vault/ SecuritySettings storeCredentials called with',
-      enabled,
-      passwordSet,
-      type,
-    );
     try {
       await Authentication.resetPassword();
 
@@ -406,13 +391,8 @@ class Settings extends PureComponent {
       await AsyncStorage.setItem(EXISTING_USER, TRUE);
 
       if (!enabled) {
-        console.log('vault/ SecuritySettings storeCredentials not enabled');
         this.setState({ [type]: false, loading: false });
         if (type === PASSCODE_CHOICE_STRING) {
-          console.log(
-            'vault/ SecuritySettings type === PASSCODE_CHOICE_STRING',
-            type,
-          );
           await AsyncStorage.setItem(PASSCODE_DISABLED, TRUE);
         } else if (type === BIOMETRY_CHOICE_STRING) {
           await AsyncStorage.setItem(BIOMETRY_CHOICE_DISABLED, TRUE);
@@ -431,10 +411,6 @@ class Settings extends PureComponent {
         } else {
           authType = AUTHENTICATION_TYPE.PASSWORD;
         }
-        console.log(
-          'vault/ SecuritySettings storeCredentials calling Authentication.storePassword with',
-          authType,
-        );
         await Authentication.storePassword(password, authType);
       } catch (error) {
         Logger.error(error);
@@ -448,7 +424,6 @@ class Settings extends PureComponent {
 
       this.setState({ [type]: true, loading: false });
     } catch (e) {
-      console.log('vault/ SecuritySettings storeCredentials error', e);
       if (e.message === 'Invalid password') {
         Alert.alert(
           strings('app_settings.invalid_password'),
